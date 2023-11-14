9b04aa919fc1af3539bcec3a7a9087024c3cbb4c
Darya Buzhynskaya <Bujinski4u@gmail.com>
2023-11-14 14:38:44 +0000

    first add

---
+++ js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+console.log('Hey hey hey');
+
+let userName = prompt('What is your name?')
+
+alert(`Welcome to my site ${userName}! Let's play a guessing game, please answer yes/no OR y/n`);
